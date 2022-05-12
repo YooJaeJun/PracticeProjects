321e4a93a92f8d8b943819f7add503db737698b8
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-05-12 21:51:44 +0900

    Add files via upload

---
+++ SGA_C_Intensive/02_Array/02_Array.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+int main()
+{
+	int Arr[] = { 1,2,3,4,5 };
+	printf("시작 주소 \n");
+	printf("Arr : %p \n", Arr);
+	int* p = Arr;
+	printf("p : %p \n", p);
+	
+	printf("시작 주소 간접 접근 \n");
+	printf("&Arr[0] : %p \n", &Arr[0]);
+	printf("*(&Arr[0]) : %i \n", *(&Arr[0]));
+	printf("*p : %i \n", *p);
+	printf("*Arr : %i \n", *Arr);
+
+	printf("\n");
+
+	printf("주소 연산 \n");
+	for(int i=0;i<5;++i)
+		printf("&Arr[%i] : %i \n", i, &Arr[i]);
+
+	printf("\n");
+	printf("각 주소에 간접 접근 \n");
+	for (int i = 0; i < 5; ++i)
+	{
+		// 주소 연산
+		// 주소값 + i
+		// 주소값 + (해당 공간의 바이트 * i)
+		printf("&Arr[%i] : %i \n", i, &Arr[i]);
+		printf("&Arr[0] + %i : %i \n", i, &Arr[0] + i);
+		printf("Arr + %i : %i \n", i, Arr + i);
+		printf("p + %i : %i \n", i, p + i);
+		printf("\n");
+	}
+
+	printf("\n");
+	printf("각 주소에 간접 참조 연산자( * )로 접근 \n");
+	for (int i = 0; i < 5; ++i)
+	{
+		printf("*(&Arr[%i])     : %i \n", i, *(&Arr[i]));
+		printf("*(&Arr[0] + %i) : %i \n", i, *(&Arr[0] + i));
+		printf("*(Arr + %i)     : %i \n", i, *(Arr + i));
+		printf("*(p + %i)       : %i \n", i, *(p + i));
+		printf("\n");
+	}
+
+	printf("\n");
+	printf("[](인덱스 연산자)로 인한 접근 \n");
+	for (int i = 0; i < 5; ++i)
+	{
+		printf("Arr[%i] : %i \n", i, Arr[i]);
+		printf("p[%i]   : %i \n", i, p[i]);
+		printf("\n");
+	}
+
+	*p = 10;
+	printf("Arr[0] : %i \n", Arr[0]);
+	
+	int A = 100;
+	p = &A;
+	printf("*p : %i \n", *p);
+	
+	int Arr2[5] = { 10, 20, 30, 40, 50 };
+	// Arr = Arr2;
+
+	// 배열은 주소값을 변경하지 못하는 상수 포인터입니다.
+
+
+
+	return 0;
+}
---
+++ SGA_C_Intensive/Test/02_Test.c
@@ -0,0 +1,28 @@
+#include "C.h"
+
+int main()
+{
+	// 아래의 해당 변수들은 어떠한 공간인지 서술해주세요.
+
+	int A;
+	// 답 : 정수자료형을 저장하고 4Byte 를 차지하며 직접접근을 통하여 값의 변경이 가능한 공간
+
+	const int A1;
+	// 답 :
+
+	int const A2;
+	// 답 :
+
+	int* P1;
+	// 답 :
+
+	const int* P2;
+	// 답 :
+
+	int* const P3;
+	// 답 :
+
+	const int* const P4;
+	// 답 :
+	return 0;
+}
