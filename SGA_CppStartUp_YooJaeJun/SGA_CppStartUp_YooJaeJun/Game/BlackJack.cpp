4432bde58251d17ce72ddca68f5301e7c03d2ba6
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-05-30 21:53:27 +0900

    Add files via upload

--- SGA_CppStartUp_YooJaeJun/SGA_CppStartUp_YooJaeJun/Game/BlackJack.cpp
+++ SGA_CppStartUp_YooJaeJun/SGA_CppStartUp_YooJaeJun/Game/BlackJack.cpp
@@ -1,166 +1,230 @@
-#include <stdio.h>
-#include <string.h>
+#include <string>
+#include <iostream>
 #include <Windows.h>
-#pragma warning(disable : 4996)
+using namespace std;
 
-typedef struct tag_Player
+struct Card
 {
-	char cards[10][5];
-	int cardCount;
-	int sum;
-} Player;
+	string Shape  = "";
+	string Number = "";
+	int    Size   = 0;
 
-void printDeck(char deck[4][13][5])
-{
-	printf("\n");
-	for (int i = 0; i < 4; i++)
+	__forceinline void Print()
 	{
-		for (int j = 0; j < 13; j++)
-		{
-			printf("%s ", deck[i][j]);
-		}
-		printf("\n");
+		cout << Shape << Number;
 	}
-}
+};
 
-void createDeck(char deck[4][13][5])
+typedef struct Player
 {
+	// 정적 멤버 변수
+	// 모든 객체들이 공유하는 멤버입니다.
+	// 전역에서 초기화를 해주셔야 합니다.
+	// 초기화 방법 : [정적멤버변수의 자료형] [설계도::변수] = value;
+	static int Index;
+	Card OwnCard[10];
+	int OwnCount = 0;
+	int Sum = 0;
+
+	void Draw(Card* deck)
+	{
+		OwnCard[OwnCount] = deck[Index];
+		++OwnCount;
+		++Index;
+	}
+
+	int Calculate()
+	{
+		Sum = 0;
+		int ACount = 0;
+		for (int i = 0; i < OwnCount; i++)
+		{
+			Sum += OwnCard[i].Size;
+			if (OwnCard[i].Size == 1) ++ACount;
+		}
+
+		if (ACount)
+		{
+			for (int i = 0; i < ACount; i++)
+			{
+				int temp = Sum;
+
+				temp += 10;
+
+				if (temp > 21) break;
+				else Sum = temp;
+			}
+		}
+
+		return Sum;
+	}
+
+	void Print()
+	{
+		cout << "소유 카드 : ";
+		for (int i = 0; i < OwnCount; i++)
+		{
+			OwnCard[i].Print();
+			cout << " ";
+		}
+		cout << endl;
+		cout << "합계 : " << Calculate() << endl;
+	}
+}Dealer;
+
+int Player::Index = 0;
+
+Card* CreateDeck()
+{
+	Card* deck = new Card[52];
+
+	int N = 0;
+
 	for (int i = 0; i < 4; i++)
 	{
 		for (int j = 0; j < 13; j++)
 		{
 			switch (i)
 			{
-			case 0: strcpy(deck[i][j], "♤");
-				break;
-			case 1: strcpy(deck[i][j], "♡");
-				break;
-			case 2: strcpy(deck[i][j], "♧");
-				break;
-			case 3: strcpy(deck[i][j], "◇");
-				break;
+			case 0: deck[N].Shape = "♤"; break;
+			case 1: deck[N].Shape = "♡"; break;
+			case 2: deck[N].Shape = "♧"; break;
+			case 3: deck[N].Shape = "◇"; break;
 			}
 
-			int num = j + 1;
+			int Num = j + 1;
 
-			switch (num)
+			deck[N].Size = Num;
+			if (deck[N].Size > 10) deck[N].Size = 10;
+
+			switch (Num)
 			{
 			case 1:
-			{
-				strcat(deck[i][j], "A");
-				break;
-			}
+				deck[N].Number = "A"; break;
 			case 11:
-			{
-				strcat(deck[i][j], "J");
-				break;
-			}
+				deck[N].Number = "J"; break;
 			case 12:
-			{
-				strcat(deck[i][j], "Q");
-				break;
-			}
+				deck[N].Number = "Q"; break;
 			case 13:
-			{
-				strcat(deck[i][j], "K");
-				break;
-			}
+				deck[N].Number = "K"; break;
 			default:
-			{
-				char str[3];
-				// strcat(str1, str2) : str1 에 str2 의 문자열을 잇습니다.
-				// itoa(value, str, expression) : 정수 형식을 문자열로 바꿉니다.
-				// - value      : 문자열로 바꿀 정수
-				// - str		: 바꾼 문자열을 저장할 배열
-				// - expression : 표현할 진수
-				strcat(deck[i][j], itoa(num, str, 10));
+				deck[N].Number = to_string(Num);
 				break;
-			}
-			}
+			}// switch
+
+			++N;
 		}
 	}
+
+	return deck;
 }
 
-void shuffle(char deck[4][13][5])
+void PrintDeck(Card* deck)
 {
-	srand(GetTickCount64());
-
-	for (int i = 0; i < 52; i++)
+	int N = 0;
+	for (int i = 0; i < 4; i++)
 	{
-		int randomIndex_1 = rand() % 4;
-		int randomIndex_2 = rand() % 13;
-		int randomIndex_3 = rand() % 4;
-		int randomIndex_4 = rand() % 13;
-
-		char temp[5] = { 0 };
-		strcpy(temp, deck[randomIndex_1][randomIndex_2]);
-		strcpy(deck[randomIndex_1][randomIndex_2], deck[randomIndex_3][randomIndex_4]);
-		strcpy(deck[randomIndex_3][randomIndex_4], temp);
-	}
-}
-
-char* draw(char deck[4][13][5], Player* player)
-{
-	// 정적변수
-	static int OneIndex = 0;
-	static int TwoIndex = 0;
-	
-	strcpy(player->cards[player->cardCount], deck[OneIndex][TwoIndex]);
-	// -> : 멤버 간접 접근 연산자
-	// (*StructAddress).Member
-	// StructAddress->member
-	//		. (멤버 직접접근 연산자) 와 다르게 구조체의 주소값에 접근하여
-	//		멤버간접접근 하는 연산자입니다.
-
-	if (++TwoIndex == 13)
-	{
-		OneIndex++;
-		TwoIndex = 0;
+		for (int j = 0; j < 13; j++)
+		{
+			deck[N].Print();
+			++N;
+		}
 		printf("\n");
 	}
-
-	++player->cardCount;
 }
 
-int calculate(Player* player)
+void Shuffle(Card* deck)
 {
-	for (int i = 0; i < player->cardCount; i++)
+	srand(GetTickCount64());
+	for (int i = 0; i < 52; i++)
 	{
-		char str[3];
-		// strlen : 널문자를 제외한 문자열의 길이를 반환합니다.
-		// strncpy(player->cards[i], ,strlen(player->cards[i]));
+		int r1 = rand() % 52;
+		int r2 = rand() % 52;
+
+		Card temp = deck[r1];
+		deck[r1] = deck[r2];
+		deck[r2] = temp;
 	}
 }
 
 int main()
 {
-	printf("♤ size : %i", sizeof(char));
-	printf("♡ size : %i", sizeof(char));
-	printf("♧ size : %i", sizeof(char));
-	printf("◇ size : %i", sizeof(char));
-	
-	
-	char deck[4][13][5];	// 행, 열, 문자열
-	createDeck(deck);
-	printDeck(deck);
-	shuffle(deck);
-	//system("pause");
-	system("cls");
-	printDeck(deck);
+	Card* Deck = CreateDeck();
+	Player* player = new Player();
+	Dealer* dealer = new Dealer();
 
-	Player player;
+	Shuffle(Deck);
 
-	// main 문에서 구조체 멤버 초기화
-
-	// deck 에서 카드를 뽑아 player의 Cards에 저장하는 함수를 만들어주세요.
-	printf("\nplayer card draw : ");
-	int t = 10;
-	while (t--)
+	int Input = 0;
+	player->Draw(Deck);
+	while (true)
 	{
-		printf("%s \n", draw(deck, &player));
+		// 플레이어 턴
+		while (true)
+		{
+			system("cls");
+			player->Print();
+			dealer->Print();
+			if (player->Calculate() >= 21) break;
+			printf("[ 1. Hit ] [ 2. Stand ] \n");
+			printf("입력 : ");
+			scanf_s("%i", &Input);
 
+			if (Input == 1)
+			{
+				player->Draw(Deck);
+				continue;
+			}
+			else if (Input == 2) break;
+			else
+			{
+				printf("값을 잘못 입력했습니다. \n");
+				Sleep(500);
+				continue;
+			}
+		}
+		if (player->Calculate() == 21)
+		{
+			cout << "Player BlackJack ! " << endl;
+			break;
+		}
+		else if (player->Calculate() > 21)
+		{
+			cout << "Player Lose" << endl;
+			break;
+		}
 
+		// 딜러 턴
+		while (true)
+		{
+			system("cls");
+			dealer->Print();
+			player->Print();
+			if (dealer->Calculate() >= 17) break;
+
+			dealer->Draw(Deck);
+
+			Sleep(500);
+		}
+		if (dealer->Calculate() > 21)
+			cout << "Dealer Lose" << endl;
+		else if (dealer->Calculate() == 21)
+			cout << "Dealer BlackJack ! " << endl;
+		else if (player->Calculate() > dealer->Calculate())
+			cout << "Player Win ! " << endl;
+		else if (player->Calculate() < dealer->Calculate())
+			cout << "Dealer Win !" << endl;
+
+		break;
 	}
 
+
+	delete player;
+	player = nullptr;
+	delete dealer;
+	dealer = nullptr;
+	delete[] Deck;
+	Deck = nullptr;
+	
 	return 0;
 }
