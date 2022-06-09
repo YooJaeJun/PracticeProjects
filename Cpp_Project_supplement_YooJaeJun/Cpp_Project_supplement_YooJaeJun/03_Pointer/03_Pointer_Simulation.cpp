341a49f29c27d647ddb81f80c844ba13b50f3072
YooJaeJun <zoflqldks123@gmail.com>
2022-06-10 00:51:32 +0900

    과제용 이미지 업로드 / 과제: 단순 전투 시뮬레이터

--- Cpp_Project_supplement_YooJaeJun/Cpp_Project_supplement_YooJaeJun/03_Pointer/03_Pointer_Simulation.cpp
+++ Cpp_Project_supplement_YooJaeJun/Cpp_Project_supplement_YooJaeJun/03_Pointer/03_Pointer_Simulation.cpp
@@ -2,6 +2,10 @@
 #include <Windows.h>
 using namespace std;
 
+// __cdecl
+// thiscall
+
+
 // 전투 자동 시뮬레이션
 
 // Player
@@ -16,52 +20,88 @@ using namespace std;
 // - 공격력
 // - 체력
 
-// 플레이어라는 사용자 정의 자료형을 선언합니다.
-struct Player
+// 유닛이라는 사용자 정의 자료형을 선언합니다.
+
+struct Unit
 {
 	// 멤버 변수는 해당 구조를 이루는 도구를 뜻합니다.
 	// 멤버 함수는 해당 구조를 이루는 기능을 뜻합니다.
+	string		Name;		// 유닛의 이름을 저장할 멤버 변수
+	unsigned	Lv;			// 유닛의 레벨을 저장할 멤버 변수
+	signed		Hp;			// 유닛의 체력을 저장할 멤버 변수
+	unsigned	Atk;		// 유닛의 공격력을 저장할 멤버 변수
 
-	string Name = "플레이어";	// 플레이어의 이름을 저장할 멤버 변수
-	unsigned Lv = 1;			// 플레이어의 레벨을 저장할 멤버 변수
-	signed Hp = 0;			// 플레이어의 체력을 저장할 멤버 변수
-	unsigned Atk = 0;			// 플레이어의 공격력을 저장할 멤버 변수
-	string Weapon = "검";		// 플레이어의 무기를 저장할 멤버 변수
+	// 유닛 멤버 기본 생성자
+	Unit()
+		: Name(""), Lv(1), Hp(0), Atk(0) {}
+	// 유닛 멤버 복사 생성자
+	Unit(const string name, const unsigned lv, const signed hp, const unsigned atk)
+		: Name(name), Lv(lv), Hp(hp), Atk(atk) {}
 
-	void swap(Player& player)
+	// 유닛 멤버들을 출력해주는 함수
+	inline void Print() const
 	{
+		// 멤버들을 모두 출력합니다.
+		cout << Name << '\n';
+		cout << "Lv     : " << Lv << '\n';
+		cout << "Hp     : " << Hp << '\n';
+		cout << "Atk    : " << Atk << '\n';
+	}
+	// 피격 프로세스
+	inline void Attack(Unit& attacker)
+	{
+		printf("--------------------------------------------------------------------\n");
+		printf("%s (이/가) 공격합니다. \n", attacker.Name.c_str());
+		HitFrom(attacker.Atk);
+
+		Sleep(700);
+		printf("%s 에게 %i 데미지 ! \n", Name.c_str(), attacker.Atk);
+		Sleep(700);
+		system("cls");
+	}
+	// 대미지 피격
+	inline void HitFrom(int damage)
+	{
+		Hp -= damage;
+		if (Hp <= 0) Hp = 0;
+	}
+};
+
+struct Player : public Unit
+{
+	string Weapon;
+
+	Player() {};
+	Player(const Unit& unit, const string weapon)
+		: Unit(unit), Weapon(weapon) {}
+
+	inline void swap(Player& player)
+	{
+		printf("--------------------------------------------------------------------\n");
 		printf("\n스왑 후 \n\n");
 		string temp = Weapon;
 		Weapon = player.Weapon;
 		player.Weapon = temp;
 	}
 
-	// 플레이어의 멤버들을 출력해주는 함수
-	void print()
+	inline void Print() const
 	{
-		// 멤버들을 모두 출력합니다.
-		cout << "                                       이름   : " << Name << '\n';
-		cout << "                                       레벨   : " << Lv << '\n';
-		cout << "                                       체력   : " << Hp << '\n';
-		cout << "                                       공격력 : " << Atk << '\n';
-		cout << "                                       무기   : " << Weapon << '\n';
-		cout << '\n';
+		printf("--------------------------------------------------------------------\n");
+		printf("                                          [ %s ]  Lv. %i \n", Name.c_str(), Lv);
+		printf("                                          Hp   : %i    Atk  : %i \n", Hp, Atk);
+		printf("                                          Weapon   : %s \n", Weapon.c_str());
 	}
 };
 
-struct Monster
+struct Monster : public Unit
 {
-	string Name = "몬스터";
-	unsigned Lv = 1;
-	signed Hp = 0;
-	unsigned Atk = 0;
+	Monster() {}
+	Monster(const Unit& unit) : Unit(unit) {}
 
-	void print()
+	inline void Print() const
 	{
-		printf("[ %s ] \n", Name.c_str());
-		printf("< Lv : %i > \n", Lv);
+		printf("[ %s ]  Lv. %i \n", Name.c_str(), Lv);
 		printf("Hp   : %i    Atk  : %i \n", Hp, Atk);
-		cout << '\n';
 	}
 };
 
@@ -70,7 +110,7 @@ int main()
 	/*
 	Player player_1;
 	Monster monster;
-	
+
 	Player player_2 = { "유재준", 1,2,3, "활" };
 	player_1.print();
 	player_2.print();
@@ -83,46 +123,27 @@ int main()
 	return 0;
 	*/
 
-	Player player;
-	player.Hp = 100;
-	player.Atk = 3;
-
-	Monster slime{ "Slime", 1, 20, 3 };
-	Monster goblin{ "Goblin", 3, 50, 5 };
-	Monster fairy{ "Fairy", 5, 100, 10 };
+	// Monster goblin{ "Goblin", 3, 50, 5 };
+	// Monster fairy{ "Fairy", 5, 100, 10 };
 
 	// 현재 플레이어만 공격하여 슬라임의 Hp가 0이 된다면 종료되는 반복문입니다.
 	// 플레이어가 공격한 후에 슬라임이 공격하도록 만들어주세요.
 
+	Player player({ "유재준", 5, 100, 5 }, "검");
+	Monster slime({ "슬라임", 20, 20, 3 });
+
 	while (slime.Hp > 0)
 	{
-		player.print();
-		slime.print();
-		
-		printf("플레이어가 공격합니다. \n");
-		slime.Hp -= player.Atk;
-		Sleep(700);
-		printf("%s 에게 %i 데미지 ! \n", slime.Name.c_str(), player.Atk);
+		player.Print();
+		slime.Print();
+		player.Attack(slime);
 
-		Sleep(700);
-		system("cls");
-
-		player.print();
-		slime.print();
-
-		printf("슬라임이 공격합니다. \n");
-		player.Hp -= slime.Atk;
-		Sleep(700);
-		printf("%s 에게 %i 데미지 ! \n", player.Name.c_str(), slime.Atk);
-
-		Sleep(700);
-		system("cls");
+		player.Print();
+		slime.Print();
+		slime.Attack(player);
 	}
 
-	cout << player.Hp > 0 ? "플레이어의 패배 !"
+	printf("--------------------------------------------------------------------\n");
+	cout << (player.Hp <= 0 ? "플레이어의 패배 ! \n" : "슬라임의 패배 ! \n");
+	printf("--------------------------------------------------------------------\n");
 }
-
-
-
-// __cdecl
-// thiscall
