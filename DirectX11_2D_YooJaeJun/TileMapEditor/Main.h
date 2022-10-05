089ab5a04871e9a821bef51d7c17c6813540a66e
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-10-05 21:48:20 +0900

    Add files via upload

--- DirectX11_2D_YooJaeJun/TileMapEditor/Main.h
+++ DirectX11_2D_YooJaeJun/TileMapEditor/Main.h
@@ -1,20 +1,23 @@
 #pragma once
+
 class Main : public Scene
 {
-public:
-	ObTileMap* map;
+private:
+	ObTileMap*	map;
 
 	Int2		pickingIdx;
 	int			imgIdx;
-
 	Int2		mouseIdx;
+
+	Int2		tileSize;
+	Color		tileColor;
+	int			tileState;
 
 public:
 	virtual void Init() override;
-	virtual void Release() override; //해제
+	virtual void Release() override;
 	virtual void Update() override;
-	virtual void LateUpdate() override;//갱신
+	virtual void LateUpdate() override;
 	virtual void Render() override;
 	virtual void ResizeScreen() override;
 };
-
