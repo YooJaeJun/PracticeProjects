5fac794c1c394d4d6ff96df09da67815afecc171
Yoo Jaejun <90949132+YooJaeJun@users.noreply.github.com>
2022-10-05 21:48:55 +0900

    Add files via upload

--- DirectX11_2D_YooJaeJun/2DFrameWork/ObTileMap.h
+++ DirectX11_2D_YooJaeJun/2DFrameWork/ObTileMap.h
@@ -1,37 +1,77 @@
 #pragma once
-
-enum TileState
+enum class TileState
 {
-    TILE_NONE,
-    TILE_WALL,
-    TILE_DOOR,
-    TILE_TRAP,
-    TILE_WATER,
-    //TILE_SAND,
-    TILE_SIZE
+    none,
+    wall,
+    door,
+    trap,
+    water,
+    tileSize
+};
+
+const int imageCount = 4;
+
+class Tile
+{
+public:
+    int         F, G, H; //타일 비용
+    Int2        idx;    //타일 인덱스
+    Tile*       P;      //나를 갱신시킨 타일
+    bool        isFind; //검사한적이 있는가?
+    TileState   state;
+    Vector2     Pos; //타일의 중점
+
+    void ClearCost();
+    void ClacH(Int2 DestIdx);
+    void ClacF();
+};
+
+using PTile = pair<Tile*, int>;
+
+struct Compare
+{
+    bool operator()(PTile& a, PTile& b)
+    {
+        return a.second > b.second;
+    }
 };
 
 class ObTileMap : public GameObject
 {
+public:
+    ObTileMap();
+
+    virtual         ~ObTileMap();
+    virtual void    CreateTileCost();
+    virtual void    ResizeTile(Int2 TileSize);
+
+    void            RenderGui(Int2& GuiPickingIdx, int& ImgIdx);
+    void            Render() override;
+    void            SetTile(Int2 TileIdx, Int2 FrameIdx, int ImgIdx = 0,
+        int TileState = none, Color color = Color(0.5f, 0.5f, 0.5f, 0.5f));
+
+    void            Save();
+    virtual void    Load();
+
+    bool            WorldPosToTileIdx(Vector2 Wpos, Int2& TileIdx);
+    Vector2         TileIdxToWorldPos(Int2 TileIdx);
+    TileState       GetTileState(Int2 TileIdx);
+    void            SetTileState(Int2 TileIdx, TileState tileState);
+    Int2            GetTileSize() { return tileSize; };
+    Vector2         GetTilePosition(Int2 TileIdx);
+
+    bool            PathFinding(Int2 sour, Int2 dest, OUT vector<Tile*>& way);
+
 protected:
     VertexTile*             vertices;
     ID3D11Buffer*           vertexBuffer;
     Int2                    tileSize;   //10x10 , 4x4
 
 public:
-    ObImage*                tileImages[4];
-    string                  file;
+    vector<vector<Tile>>    Tiles;
 
 public:
-    ObTileMap();
-
-    virtual ~ObTileMap();
-    virtual void        ResizeTile(Int2 TileSize);
-    virtual bool        WorldPosToTileIdx(Vector2 WPos, Int2& TileIdx);
-
-    void                RenderGui(Int2& GuiPickingIdx, int& ImgIdx);
-    void                Render() override;
-    void                SetTile(Int2 TileIdx, Int2 FrameIdx, int ImgIdx = 0,
-        int TileState = TILE_NONE, Color color = Color(0.5f, 0.5f, 0.5f, 0.5f));
+    ObImage*                tileImages[imageCount];
+    string                  file;
 };
 
